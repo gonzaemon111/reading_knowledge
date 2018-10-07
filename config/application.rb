358dc48cfdb83eaa75385f6a5ce809ba2839f943
gonzaemon111 <goki14914@icloud.com>
2018-10-08 02:58:06 +0900

    add configuration for rspec

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -21,13 +21,26 @@ module ReadingKnowledge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = "Tokyo"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: true, # モデル作成時にフィクスチャの作成を有効化(後述のfactory_botが適用される)
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+
+      # fixtureの代わりにfactory_botを使うよう設定
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
     config.generators.system_tests = nil
   end
 end
