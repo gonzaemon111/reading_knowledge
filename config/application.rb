deb75c6c0106b4962997a1028ed7dc9a8434a20b
gonzaemon111 <goki14914@icloud.com>
2018-10-08 04:07:48 +0900

    add devise:user for first step

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,12 @@ module ReadingKnowledge
       g.assets false
       g.helper false
       g.test_framework :rspec,
-                       fixtures: true, # モデル作成時にフィクスチャの作成を有効化(後述のfactory_botが適用される)
+                       fixtures: false, # モデル作成時にフィクスチャの作成を有効化(後述のfactory_botが適用される)
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
                        controller_specs: false,
-                       request_specs: false
+                       request_specs: true
 
       # fixtureの代わりにfactory_botを使うよう設定
       g.fixture_replacement :factory_bot, dir: "spec/factories"
